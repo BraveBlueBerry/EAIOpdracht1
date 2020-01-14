bc54e8f1325b0c48c0b4fb50bf6d742aea8b6b12
Talitha Voortman <t.g.i.voortman@st.hanze.nl>
2020-01-14 12:55:04 +0100

    Initial commit with project.

---
+++ src/hanze/nl/infobord/QueueListener.java
@@ -0,0 +1,19 @@
+package hanze.nl.infobord;
+
+import java.io.IOException;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+public class QueueListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+
+    }
+//TODO 	implementeer de messagelistener die het bericht ophaald en
+//		doorstuurd naar verwerkBericht van het infoBord.
+//		Ook moet setRegels aangeroepen worden.
+}
+
